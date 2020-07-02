b2450efbab755aaa1f831ae4586c1e1477849132
Rithie Natan <rithienatan@gmail.com>
2020-07-02 19:24:47 -0300

    Updates

---
+++ precache-manifest.23fe443e1f131ba970e205d37f168528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4144fcf477cb97eb63d80f4cdf3af16",
+    "url": "/website-portfolio-template/index.html"
+  },
+  {
+    "revision": "a4b5d4e0127288bc08f5",
+    "url": "/website-portfolio-template/static/css/main.78136bf0.chunk.css"
+  },
+  {
+    "revision": "aad3e6fd68109a40ceb8",
+    "url": "/website-portfolio-template/static/js/2.b50f5a99.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/website-portfolio-template/static/js/2.b50f5a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b5d4e0127288bc08f5",
+    "url": "/website-portfolio-template/static/js/main.e8185eae.chunk.js"
+  },
+  {
+    "revision": "30146b45eb7f6bb05831",
+    "url": "/website-portfolio-template/static/js/runtime-main.ca598ceb.js"
+  }
+]);
